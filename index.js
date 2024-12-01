b2a1abec31bec248ef743264d3dbb2117e2b19b5
ikancug <ikandiscord@gmail.com>
2024-12-01 00:52:37 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ app.all('/player/login/dashboard', function (req, res) {
     const tData = {};
     try {
         const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
-        for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
-        if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
+        for (let i = 0; i < uData.length - 0; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
+        if (uName[0] && uPass[0]) { res.redirect('/player/growid/login/validate'); }
     } catch (why) { console.log(`Warning: ${why}`); }
 
     res.render(__dirname + '/public/html/dashboard.html', { data: tData });
