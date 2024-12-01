8e450fd331924b1f17bba7a2f5ecdaa086ded422
ikancug <ikandiscord@gmail.com>
2024-12-01 00:48:31 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -30,6 +30,13 @@ app.use(express.json());
 app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
 
 app.all('/player/login/dashboard', function (req, res) {
+    const tData = {};
+    try {
+        const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
+        for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
+        if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
+    } catch (why) { console.log(`Warning: ${why}`); }
+
     res.render(__dirname + '/public/html/dashboard.html', { data: tData });
 });
 
@@ -47,11 +54,8 @@ app.all('/player/growid/login/validate', (req, res) => {
     );
 });
 
-//app.get('/', function (req, res) {
-   // res.send('helo world');
-//});
-app.all('/player/login/dashboard', function (req, res) {
-    res.render(__dirname + '/public/html/dashboard.html', { data: tData });
+app.get('/', function (req, res) {
+    res.send('Hello World!');
 });
 
 app.listen(5000, function () {
